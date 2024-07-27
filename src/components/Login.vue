76ffbd518d56199b068e29397fbc65789cd0b0c7
Miharu669 <veronicadoelfuentes@gmail.com>
2024-07-27 05:41:28 +0200

    changed details on login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -25,19 +25,18 @@ export default {
       </div>
 
       <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-        <h1 class="text-2xl font-semibold mb-4">Login</h1>
         <form @submit.prevent="login">
           <div class="mb-4">
             <label for="username" class="block text-gray-600">Username</label>
             <input type="text" id="username" v-model="username" requiered
-              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-cyan-500"
               autocomplete="off" />
           </div>
 
           <div class="mb-4">
             <label for="password" class="block text-gray-600">Password</label>
             <input type="password" id="password" v-model="password" required
-              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-amber-500"
               autocomplete="off" />
           </div>
           <div class="bg-transparent">
